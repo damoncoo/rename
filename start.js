a86502367daac9a03176753d35e31cdeacc963dc
Damon Coo <damoncoo@gmail.com>
2019-10-18 23:23:53 +0800

    rerange code

--- start.js
+++ start.js
@@ -22,20 +22,17 @@ program
   .option('-n, --new [type]', 'new prefix')
   .action((project, cmdObj) => {
     console.log('Renameing Images...')
-
     image.renameImages(project, {
       old: cmdObj.old,
       new: cmdObj.new,
       image: function (images) {
         let filtered = images.filter((classFile) => !/(\.framework|Pods)/.test(classFile))
         return filtered
-      },
-      classes: function (classes) {
+      }, classes: function (classes) {
         let filtered = classes.filter((classFile) => !/(\.xcassets|Pods|\.framework)/.test(classFile))
         return filtered
       },
     })
-
   })
 
 program
@@ -44,20 +41,17 @@ program
   .option('-n, --new [type]', 'new prefix')
   .option('-e, --extra [type]', 'extra classes')
   .action((project, cmdObj) => {
-
     console.log('Renameing Classes...')
     classes.renameClasses(project, {
       old: cmdObj.old,
-      new: cmdObj.new
+      new: cmdObj.new      
     }, function (classes) {
-
       let filtered = classes.filter((classFile) => !/(\.framework)|(Pods)/.test(classFile))
       if (cmdObj.extra && cmdObj.extra.length) {
         filtered.push(cmdObj.extra)
       }
       return filtered
     })
-
   })
 
 
